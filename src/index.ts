6c60724315a2a98dad3ad46b3430ca67f0612fcd
Alex Knott <alextknott@gmail.com>
2023-02-23 17:12:48 +0000

    Register tracking router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { fastify } from "fastify";
 
+import tracking from "./routes/tracking.js";
+
 const server = fastify();
+
+await server.register(tracking, { prefix: "/tracking" });
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err instanceof Error) {
