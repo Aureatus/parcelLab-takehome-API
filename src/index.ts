e8c6bcd69cea040d9f325a8b0eeb2a62cdda960e
Alex Knott <alextknott@gmail.com>
2023-02-24 16:57:59 +0000

    Turn on logging, and use pino-pretty

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,22 @@ import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
 import tracking from "./routes/tracking.js";
 
-const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
+const server = fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+}).withTypeProvider<TypeBoxTypeProvider>();
 
 await server.register(tracking, { prefix: "/tracking" });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080 }, (err) => {
   if (err instanceof Error) {
     console.error(err);
   }
-  console.log(`Server listening at ${address}`);
 });
