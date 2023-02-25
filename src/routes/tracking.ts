34028dfdd298ba4efea1377651b55ec5f3b21972
Alex Knott <alextknott@gmail.com>
2023-02-25 16:55:05 +0000

    Use preValidator hook to remove additional properties
    Easier approach should be the TypeBox additionalProperties boolean
    property, but it throws errors for them when using
    TypeBoxValidatorCompiler rather than removing them. This seems the
    better approach.

--- src/routes/tracking.ts
+++ src/routes/tracking.ts
@@ -7,18 +7,15 @@ const carrierCodes = JSON.parse(
   await readFile("src/data/carrier_codes.json", "utf8")
 ) as string[];
 
-const TrackingData = Type.Object(
-  {
-    courier: Type.Union(carrierCodes.map((code) => Type.Literal(code))), // Perhaps improve by using custom AJV format....
-    tracking_number: Type.String(),
-    zip_code: Type.String(), // Validate this in a pre-handler using regex perhaps, or with a call to some sort of postcode validation API. Or with custom AJV format.
-    destination_country_iso3: Type.String(), // Validate this in a custom AJV format using country-code-lookup.
-    return: Type.Optional(Type.Boolean()),
-    cancelled: Type.Optional(Type.Boolean()),
-    notificationsInactive: Type.Optional(Type.Boolean()),
-  },
-  { additionalProperties: false }
-);
+const TrackingData = Type.Object({
+  courier: Type.Union(carrierCodes.map((code) => Type.Literal(code))), // Perhaps improve by using custom AJV format....
+  tracking_number: Type.String(),
+  zip_code: Type.String(), // Validate this in a pre-handler using regex perhaps, or with a call to some sort of postcode validation API. Or with custom AJV format.
+  destination_country_iso3: Type.String(), // Validate this in a custom AJV format using country-code-lookup.
+  return: Type.Optional(Type.Boolean()),
+  cancelled: Type.Optional(Type.Boolean()),
+  notificationsInactive: Type.Optional(Type.Boolean()),
+});
 
 type TrackingType = Static<typeof TrackingData>;
 
@@ -26,6 +23,15 @@ const tracking = async (fastify: FastifyInstance) => {
   fastify.post<{ Body: TrackingType }>(
     "/",
     {
+      preValidation: (req, _reply, done) => {
+        const { properties } = TrackingData;
+        const desiredPropertyKeys = Object.keys(properties);
+        for (const prop in req.body) {
+          if (!desiredPropertyKeys.includes(prop))
+            delete req.body[prop as keyof TrackingType];
+        }
+        done();
+      },
       schema: {
         body: TrackingData,
       },
