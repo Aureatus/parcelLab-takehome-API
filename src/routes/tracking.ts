7256be47fe4623dd78fc837f3a0c974ef5ac5d1d
Alex Knott <alextknott@gmail.com>
2023-02-27 14:03:51 +0000

    Add route that allows files to be used.
    Currently only supports JSON

--- src/routes/tracking.ts
+++ src/routes/tracking.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Static, Type } from "@sinclair/typebox";
 import { TypeSystem } from "@sinclair/typebox/system";
 import { readFile } from "node:fs/promises";
@@ -28,6 +29,12 @@ const TrackingData = Type.Object({
 
 type TrackingType = Static<typeof TrackingData>;
 
+const FileParametersSchema = Type.Object({
+  type: Type.String(),
+});
+
+type FileParametersType = Static<typeof FileParametersSchema>;
+
 const tracking = async (fastify: FastifyInstance) => {
   fastify.post<{ Body: TrackingType }>(
     "/",
@@ -48,6 +55,32 @@ const tracking = async (fastify: FastifyInstance) => {
     (req) => {
       console.log(req.body);
       return "tracking";
+    }
+  );
+
+  fastify.post<{ Body: TrackingType; Params: FileParametersType }>(
+    "/file/:type",
+    {
+      preValidation: async (req) => {
+        const data = await req.file();
+        if (data === undefined) throw new Error("No file uploaded"); // Currently sends 500 status code, should be 400. Can potentially fix with setErrorHandler
+        const dataBuffer = await data.toBuffer(); // Throws error if file is over allowed size.
+        const file = dataBuffer.toString();
+        if (data.mimetype === "application/json") {
+          const information = JSON.parse(file) as TrackingType; // Not ideal, it's not a good assertion to have.
+          req.body = information;
+          const { properties } = TrackingData;
+          const desiredPropertyKeys = Object.keys(properties);
+          for (const prop in req.body) {
+            if (!desiredPropertyKeys.includes(prop))
+              delete req.body[prop as keyof TrackingType];
+          }
+        }
+      },
+      schema: { body: TrackingData, params: FileParametersSchema },
+    },
+    async (req) => {
+      return req.body;
     }
   );
 };
