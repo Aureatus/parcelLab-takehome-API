9abdae3f475b534d41357535cb4a609d2c5bfb47
Alex Knott <alextknott@gmail.com>
2023-02-28 10:27:32 +0000

    Add support for more delimiters
    Haven't had time to test this, but it seems sound.

--- src/helpers/parse-csv.ts
+++ src/helpers/parse-csv.ts
@@ -3,20 +3,23 @@ import type { TrackingType, FileTrackingType } from "../schema/tracking.js";
 
 const parseCSV = async (file: string) => {
   const data: FileTrackingType = [];
+  const delimiters = [",", ";", "|", "+", "\t"];
   await new Promise<void>((resolve) => {
-    csv
-      .parseString(file, {
-        objectMode: true,
-        delimiter: ";",
-        headers: true,
-        ignoreEmpty: true,
-        trim: true,
-      })
-      .on("error", (error) => console.error(error))
-      .on("data", (row) => data.push(row as TrackingType))
-      .on("end", () => {
-        resolve();
-      });
+    for (const test of delimiters) {
+      if (data.length > 0) break;
+      csv
+        .parseString(file, {
+          objectMode: true,
+          delimiter: test,
+          headers: true,
+          ignoreEmpty: true,
+          trim: true,
+        })
+        .on("data", (row) => data.push(row as TrackingType))
+        .on("end", () => {
+          resolve();
+        });
+    }
   });
   return data as unknown;
 };
